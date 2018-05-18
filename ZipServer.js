9cb91dc66633f07c313fdfc34d79144186d6a1a8
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-05-17 18:45:55 -0700

    Some clean up

--- ZipServer.js
+++ ZipServer.js
@@ -106,10 +106,10 @@ class ZipServer {
 
         }
 
-        // remove the buffer
-        this._serviceWorker.postMessage({ id, buffer: null });
+        // remove the buffer on the next frame so requests have a chance to resolve
+        requestAnimationFrame(() => this._serviceWorker.postMessage({ id, buffer: null }));
         this._ids.splice(index, 1);
-
+        
     }
 
     clearAll() {
